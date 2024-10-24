cc4a995ea383b819fdf902b940c50557eab36448
Kwangjun Kim <kwangjunkim@softverse.kr>
2024-10-24 15:26:02 +0000

    20241025

--- owner_lee/create_layout.py
+++ owner_lee/create_layout.py
@@ -103,15 +103,40 @@ def main():
           # 최소 1명의 작업자가 이 작업을 성공적으로 완료하여야 합니다.
           # 지금은 kang의 개인키만 허용하였지만, 허용된 개인키가 다수일 경우 의미가 있는 숫자입니다.               
           "threshold": 1,
+        },{
+          # 네 번째 단계의 이름은 generate-sbom 입니다. 이 단계에서는 SBOM을 서명합니다.
+          "name": "generate-sbom",
+          # 이 단계에서는 (update-version)단계에서 수정된 파일과 (package)단계에서 압축된 파일이 필요합니다. 그 외엔 모두 DISALLOW
+          "expected_materials": [
+            ["MATCH", "demo-project/*", "WITH", "PRODUCTS", "FROM",
+             "update-version"], 
+            ["MATCH", "demo-project.tar.gz", "WITH", "PRODUCTS", "FROM",
+             "package"], 
+            ["DISALLOW", "*"]
+          ],
+          # 이 단계에서는 결과물로 sbom.json 파일이 생성됩니다. 그 외엔 모두 DISALLOW
+          "expected_products": [
+              ["CREATE", "sbom.json"], ["DISALLOW", "*"],
+          ],
+          # 이 단계는 kim의 개인키를 가진 사람만 수행할 수 있습니다. kim의 개인키로 서명합니다.
+          "pubkeys": [key_carl["keyid"]],
+          # 이 단계에서 실행될 명령은 지정되지 않았습니다.
+          # 즉, SBOM 생성 도구는 자유롭게 선택 및 사용 가능합니다.
+          "expected_command": [],
+          # 최소 1명의 작업자가 이 작업을 성공적으로 완료하여야 합니다.
+          # 지금은 kang의 개인키만 허용하였지만, 허용된 개인키가 다수일 경우 의미가 있는 숫자입니다.             
+          "threshold": 1,
         }],
       # inspect: 검사를 수행 내용을 정의합니다.
       "inspect": [{
           # 검사의 이름은 untar 입니다. 파일을 압축 해제하는 검사를 수행합니다.
           "name": "untar",
-          # 이 단계에서는 package 단계에서 생성된 demo-project.tar.gz 파일이 필요합니다.
+          # 이 단계에서는 package 단계에서 생성된 demo-project.tar.gz 파일과
+          # generate-sbom 단계에서 생성된 sbom.json이 필요합니다.
           # 기본적으로 가지고 있던 .keep 파일과 owner로 부터 받은 lee.pub, root.layout 그리고 link 파일들만 허용합니다. 그 외엔 모두 DISALLOW
           "expected_materials": [
               ["MATCH", "in-toto-demo-project.tar.gz", "WITH", "PRODUCTS", "FROM", "package"],
+              ["MATCH", "sbom.json", "WITH", "PRODUCTS", "FROM", "generate-sbom"],
               ["ALLOW", ".keep"],
               ["ALLOW", "lee.pub"],
               ["ALLOW", "root.layout"],
@@ -119,12 +144,14 @@ def main():
               ["DISALLOW", "*"]
           ],
           # 압축이 해제된 후에는 foo.py 파일과 기타 허용(ALLOW)된 파일만 존재해야 합니다.
-          # 압축이 해제되면, update-version 단계에서 수정된 foo.py 파일과 git 관련 파일, tar.gz 압축파일,
+          # 압축이 해제되면, update-version 단계에서 수정된 foo.py 파일과 git 관련 파일, tar.gz 압축파일, 
+          # generate-sbom 단계에서 생성된 sbom.json 파일
           # 그리고 기본적으로 가지고 있던 .keep 파일과 owner로 부터 받은 lee.pub, root.layout 그리고 link 파일들만 있어야합니다. 그 외엔 모두 DISALLOW
           "expected_products": [
               ["MATCH", "in-toto-demo-project/foo.py", "WITH", "PRODUCTS", "FROM", "update-version"],
               ["ALLOW", "in-toto-demo-project/.git/*"],
               ["ALLOW", "in-toto-demo-project.tar.gz"],
+              ["ALLOW", "sbom.json"],
               ["ALLOW", ".keep"],
               ["ALLOW", "lee.pub"],
               ["ALLOW", "root.layout"],
